dbe599c38ecdb41aefc0a98a0fb6be7df85d8a96
trollric <90m.hansson@gmail.com>
2018-01-02 08:18:02 +0100

    most recent

--- RefTypeValTypeParams/RefTypeValTypeParams/Program.cs
+++ RefTypeValTypeParams/RefTypeValTypeParams/Program.cs
@@ -10,6 +10,23 @@ namespace RefTypeValTypeParams
     {
         static void Main(string[] args)
         {
+            // Passing ref-types by value
+            Console.WriteLine("****** Passing Person Object by Value ******");
+            Person fred = new Person("Fred", 12);
+            Console.WriteLine("\nBefore by value call, Person is:");
+            fred.Display();
+            SendApersonByValue(fred);
+            Console.WriteLine("\nAfter by value call:");
+            fred.Display();
+            Console.ReadLine();
+        }
+
+        static void SendApersonByValue(Person p)
+        {
+            // Change the age of p
+            p.personAge = 99;
+            // Will the caller see this reassignment?
+            p = new Person("Nikki", 80);
         }
     }
 }
